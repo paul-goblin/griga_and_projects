6cd6a5a0f196d1c5875f4f0f24f49e1ed86d997b
paul-goblin <42851996+paul-goblin@users.noreply.github.com>
2021-01-24 21:51:30 +0100

    started working on ghosty again

--- src/ghosty/app.js
+++ src/ghosty/app.js
@@ -20,11 +20,38 @@ const grigaConfig = {
 
 class App {
   constructor() {
+    this.home_button = document.querySelector('.home-button');
+    this.play_button = document.querySelector('.play-button');
+    this.editor_button = document.querySelector('.editor-button');
+    this.content_div = document.querySelector('.content');
     this.griga = new Griga( grigaConfig, griga => this.startGame( griga ) );
+    this.setupEventListeners()
   }
 
   startGame( griga ){
     console.log( griga );
+  }
+
+  handleHomeButtonClick(){
+    console.log('home-button clicked')
+  }
+
+  handlePlayButtonClick(){
+    console.log('play-button clicked')
+  }
+  
+  handleEditorButtonClick(){
+    console.log('editor-button clicked')
+  }
+  
+  handleContentDivClick(){
+    console.log('content-div clicked')
+  }
+  setupEventListeners(){
+    this.home_button.addEventListener('click', e => this.handleHomeButtonClick())
+    this.play_button.addEventListener('click', e => this.handlePlayButtonClick())
+    this.editor_button.addEventListener('click', e => this.handleEditorButtonClick())
+    this.content_div.addEventListener('click', e => this.handleContentDivClick())
   }
 }
 
